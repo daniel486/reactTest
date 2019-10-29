50eb0ebe9fd95ee1b4f50e567f498bee2ef1f183
Daniel F Garcia <daniel.garcia@10pearls.com>
2019-10-29 10:08:51 -0500

    Changing the state of the square

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">
-        {this.props.value}
-    </button>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
 
